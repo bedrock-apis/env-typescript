bda692e18143af1a897e891f9eccb3daf8352819
con <conmaster@noreply.com>
2025-10-31 12:44:06 +0100

    Should work

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { system } from "@minecraft/server";
-
-system.run(() => {
-    console.warn("Hello, Minecraft Bedrock Edition!");
-})
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,9 +1,10 @@
-declare module globalThis {
-    var print: (...data: any[]) => void;
-    var console: {
+declare type Console = {
         log: (...data: any[]) => void;
         warn: (...data: any[]) => void;
         error: (...data: any[]) => void;
         info: (...data: any[]) => void;
-    }
-}
+};
+declare var console: Console;
+declare var print: Console["info"];
+
+declare var __clock_time: ()=>number;
